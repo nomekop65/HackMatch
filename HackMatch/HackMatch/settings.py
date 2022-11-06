b6a3cf731ca504b6bf1d304c9d99095a8e666d03
Dennis Martin <56493073+denniseugenemartin@users.noreply.github.com>
2022-11-06 03:43:31 -0500

    setup local mssql database

--- HackMatch/HackMatch/settings.py
+++ HackMatch/HackMatch/settings.py
@@ -14,11 +14,6 @@ from pathlib import Path
 import os
 import django.contrib.staticfiles
 
-
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
